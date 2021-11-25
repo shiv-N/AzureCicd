11399f7f20a1b389904999d4933c341cf761a055
Saurabh Navdkar <saurabh.navdkar@gmail.com>
2021-11-25 19:07:22 +0530

    Initial commit

---
+++ democicdazure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace democicdazure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
